3fc377520e9263f6c8d8ba4dcf00217d00312b47
bflaherty88 <bflaherty88@gmail.com>
2013-09-17 19:51:51 -0400

    Added over the shoulder camera controls

--- My First Unity Game/Assets/Scripts/CharacterControl.cs
+++ My First Unity Game/Assets/Scripts/CharacterControl.cs
@@ -1,47 +1,41 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterControl))]
 public class CharacterControl : MonoBehaviour
 {
-    public Control MoveForward;
-    public Control MoveBackward;
-    public Control TurnLeft;
-    public Control TurnRight;
-    public Control Jump;
+    CharacterController controller;
 
     public float moveSpeed = 5f;
     public float rotSpeed = 100f;
     public float jumpHeight = 8f;
+    public float jumpBoost = 1.5f;
     public float gravity = 20f;
 
     private Vector3 moveDirection = Vector3.zero;
 
-    /*    private float trueSpeed
-        {
-            get { return (moveSpeed * Time.deltaTime); }
-        }
-
-        private float trueRotSpeed
-        {
-            get { return (rotSpeed * Time.deltaTime); }
-        }
-    */
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-            if(Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump"))
+            {
                 moveDirection.y = jumpHeight;
+                moveDirection.z *= jumpBoost;
+            }
         }
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * moveSpeed * Time.deltaTime);
 
-        transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Horizontal"));
+        transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
     }
 
 }
